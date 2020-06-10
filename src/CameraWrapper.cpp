87f9b9a709f601315099272dec37928ef7a4fb1f
huajian1069 <huajian1069@gmail.com>
2020-06-10 23:46:18 +0200

    WIP: change rotation of mesh to rotation of camera

--- src/CameraWrapper.cpp
+++ src/CameraWrapper.cpp
@@ -1,8 +1,14 @@
 #include "headers/CameraWrapper.h"
+#include <Qt3DCore/qtransform.h>
 
 CameraWrapper::CameraWrapper(Qt3DCore::QEntity *rootEntity,  Qt3DRender::QCamera *camera) : m_rootEntity(rootEntity)
 {
     m_camera = camera;
+    m_cameraTransform = new Qt3DCore::QTransform();
+    m_cameraTransform->setScale(1.0f);
+    m_cameraTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(0.0f, 1.0f, 0.0f), 0.0f));
+    m_cameraTransform->setTranslation(QVector3D(0.0f, 0.0f, 20.0f));
+    m_camera->addComponent(m_cameraTransform);
 }
 
 void CameraWrapper::setProjectiveMode(bool isPerspective){
@@ -24,4 +30,26 @@ void CameraWrapper::enableCameraController(bool isEnble){
         m_camController ->setCamera(nullptr);
     else
         m_camController ->setCamera(m_camera);
+}
+
+void CameraWrapper::scaleView(int magnitude){
+    float magnitudeF = (float)(magnitude - 30);
+   // m_camera->setPosition(QVector3D(0, 0, magnitudeF));
+    m_cameraTransform->setTranslation(QVector3D(0, 0, magnitudeF));
+    qInfo() << "position" << m_camera->position() << endl;
+}
+
+void CameraWrapper::rotateViewX(int degree){
+    float degreeF = degree * 360.0 / 100.0;
+    m_cameraTransform ->setRotationX(degreeF);
+}
+
+void CameraWrapper::rotateViewY(int degree){
+    float degreeF = degree * 360.0 / 100.0;
+    m_cameraTransform->setRotationY(degreeF);
+}
+
+void CameraWrapper::rotateViewZ(int degree){
+    float degreeF = degree * 360.0 / 100.0;
+    m_cameraTransform->setRotationZ(degreeF);
 }
