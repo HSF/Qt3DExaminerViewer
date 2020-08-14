4b5dbd9b14bb5dbd96fa42a99ff83d69f368d48f
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-08-14 19:07:34 +0200

    add sources about setting picking correctly

--- main.cpp
+++ main.cpp
@@ -70,6 +70,7 @@ int main(int argc, char **argv){
     view->activeFrameGraph()->dumpObjectTree();
     
     // view's picking settings
+    // see: https://stackoverflow.com/a/58412885/32036
     auto rendersettings=view->renderSettings();
     rendersettings->pickingSettings()->setPickMethod(Qt3DRender::QPickingSettings::TrianglePicking);
     rendersettings->pickingSettings()->setPickResultMode(Qt3DRender::QPickingSettings::AllPicks);
